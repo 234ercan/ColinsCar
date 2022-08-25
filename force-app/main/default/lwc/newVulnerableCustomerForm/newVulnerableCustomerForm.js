9cf7a770598d51e36c75fb0554cc56105d8abe8a
Ercan <234ercan@gmail.com>
2022-08-25 13:04:54 +0100

    hi

--- force-app/main/default/lwc/newVulnerableCustomerForm/newVulnerableCustomerForm.js
+++ force-app/main/default/lwc/newVulnerableCustomerForm/newVulnerableCustomerForm.js
@@ -1,12 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import FirstName_FIELD from '@salesforce/schema/Vulnerable_Form__c.Name__c';
 import saveRegRecord from '@salesforce/apex/createRecord.createRecord1';
+import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+import AccountName from '@salesforce/schema/Vulnerable_Form__c.Customer_Name__c';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 export default class NewVulnerableCustomerForm extends LightningElement {
   regRecord = {
     fName : FirstName_FIELD
 };
-
+  regAccount = {
+    AccName : [AccountName]
+  }; 
 handleChange(event) {
     const field = event.target.name;
     if (field === 'txtFname') {
@@ -14,7 +19,6 @@ handleChange(event) {
         this.regRecord.fName = event.target.value;            
     } 
 }
-
 saveRecord(event)
 {   
     saveRegRecord({reg : this.regRecord})
@@ -30,5 +34,26 @@ saveRecord(event)
         console.log('error==>'+this.error);
         alert('record not inserted');
     });
+}
+saveAccount(event)
+{   
+    saveAccRecord({acc : this.regAccount})
+    .then(result => {
+        // Clear the user enter values
+        console.log(this.regAccount)
+        this.conrec = {};            
+        window.console.log('result ===> '+result);
+        alert('record inserted');
+    })
+    .catch(error => {
+        this.error = error.message;
+        console.log('error==>'+this.error);
+        alert('record not inserted');
+    });
+}
+filterWord;
+@wire(getAccounts,{filter:'$filterWord'}) accountList;
+handleSearch(event){
+    this.filterWord=event.target.value;
 }
 }
